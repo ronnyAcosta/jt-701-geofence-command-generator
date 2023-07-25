b0c32b0e9e4bf43de1dbc4844461d79e7ae095a0
ronnyAcosta <racosta0254@gmail.com>
2023-07-25 15:41:39 -0400

    command added

--- src/App.jsx
+++ src/App.jsx
@@ -9,64 +9,94 @@ const App = () => {
   const [center, setCenter] = useState ({ lat: 18.4821, lng: -69.9099 });
   const ZOOM_LEVEL = 12;
   const mapRef = useRef();
-  const [polygons, setPolygons] = useState([]); 
+  const [polygons, setPolygons] = useState([]);
+
+  const editedRef = useRef(false);
+  const deletedRef = useRef(false);
   
+  const ddddToDdmm = (coordinate) =>{
+    coordinate = String(coordinate).split(".");
+    let dec = (Number(`0.${coordinate[1]}`)*60).toFixed(4);
+    if(dec < 10){
+        coordinate[1] = String(`0${dec}`);
+    }
+    else{
+        coordinate[1] = String(dec);
+    }
+    return `${coordinate[0]}${coordinate[1]}`
+}
 
+  const getCommand = (coordinates, index) =>{
+    let command = `(P29,1,${index},1,${coordinates.length}`;
+    coordinates.map(c => {
+      command += `,${ddddToDdmm(c.lng)},${ddddToDdmm(c.lat)}`;
+  });
+    command += ')';
+    console.log(command);
+    return command;
+}
 
-  const _created = (e) => {
+  const _created = (e) => {  
     const newPolygon = { _id: e.layer._leaflet_id};
     newPolygon.coordinates = e.layer._latlngs[0].map((latlng) => ({
       lat: latlng.lat,
       lng: latlng.lng,
     }));
 
-    setPolygons((polygons) => [...polygons, newPolygon]);
+    setPolygons((polygons) => [...polygons, newPolygon]);  
     console.log(e);
+    
   }
 
   const _edited = (e) => {
-    console.log(e);
-    const { layers: {_layers}} = e;
-
-    const editedIds = Object.values(_layers).map(({ _leaflet_id}) => _leaflet_id );
-    console.log(editedIds);
-    
-    setPolygons((polygons) => {
-      polygons.map( (p) =>{
-        editedIds.map(id =>{
-          if(p._id === id){
-            p.coordinates = _layers[id]._latlngs[0];
-          }
+    if (!editedRef.current) {
+      editedRef.current = true;
+      
+      console.log(e);
+      const { layers: {_layers}} = e;
+  
+      const editedIds = Object.values(_layers).map(({ _leaflet_id}) => _leaflet_id );
+      console.log(editedIds);
+      
+      setPolygons((polygons) => {
+        polygons.map( (p) =>{
+          editedIds.map(id =>{
+            if(p._id === id){
+              p.coordinates = _layers[id]._latlngs[0];
+            }
+          }) 
         }) 
-      }) 
-      return [...polygons];
-    })
-    
+        return [...polygons];
+      })
+    }   
   };
  
-
   const _deleted = (e) => {
-    console.log(e);
-    const { layers: {_layers}} = e
-
-    const deletedIds = Object.values(_layers).map(({ _leaflet_id}) => _leaflet_id )
-    console.log(deletedIds)
-    
-    setPolygons((polygons) => {
-      polygons.map( (p) =>{
-        deletedIds.map(id =>{
-          if(p._id === id){
-            polygons.splice(polygons.indexOf(p),1);
-          }
+    if (!deletedRef.current) {
+      console.log(e);
+      deletedRef.current = true;
+      
+      const { layers: {_layers}} = e;
+      const deletedIds = Object.values(_layers).map(({ _leaflet_id}) => _leaflet_id )
+      
+      setPolygons((polygons) => {
+        polygons.map( (p) =>{
+          deletedIds.map(id =>{
+            if(p._id === id){
+              polygons.splice(polygons.indexOf(p),1);
+            }
+          }) 
         }) 
-      }) 
-      return [...polygons];
-    })
+        return [...polygons];
+      });     
+    }
   };
   
   useEffect(() => {
-    console.log(polygons); // Check if polygons state is being updated
-  }, [polygons]);
+    editedRef.current = false;
+    deletedRef.current = false;
+    console.log(polygons);
+  });
 
   return(
     <>
@@ -81,13 +111,13 @@ const App = () => {
                   position="topright" 
                   onCreated={_created}
                   onEdited={_edited}
-                  onDeleted={_deleted} 
+                  onDeleted={_deleted}
                   draw={{
                     rectangle: false,
                     circle: false,
                     circlemarker: false,
                     marker: false,
-                    polyline: false  
+                    polyline: false
                   }}
                 />
               </FeatureGroup>
@@ -101,11 +131,11 @@ const App = () => {
       </div>
 
       <div>
-        <h3>Polygons Information:</h3>
+        <h3>Geofences Commands:</h3>
         <ul>
           {polygons.map((polygon, index) => (
             <li key={index}>
-              Polygon {index + 1}: {JSON.stringify(polygon)}
+              Geofence {index + 1}: {getCommand(polygon.coordinates, index+1)}
             </li>
           ))}
         </ul>
