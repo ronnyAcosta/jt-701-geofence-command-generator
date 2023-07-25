eab13222ba2c73d445126d1001e06c76ab06bb95
ronnyAcosta <racosta0254@gmail.com>
2023-07-25 16:30:57 -0400

    Limits added

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,12 @@ const App = () => {
   const [center, setCenter] = useState ({ lat: 18.4821, lng: -69.9099 });
   const ZOOM_LEVEL = 12;
   const mapRef = useRef();
-  const [polygons, setPolygons] = useState([]);
+  const [geofences, setGeofences] = useState([]);
 
   const editedRef = useRef(false);
   const deletedRef = useRef(false);
   
+  const COMMANDS_QTY = 10;
   const ddddToDdmm = (coordinate) =>{
     coordinate = String(coordinate).split(".");
     let dec = (Number(`0.${coordinate[1]}`)*60).toFixed(4);
@@ -27,6 +28,11 @@ const App = () => {
 }
 
   const getCommand = (coordinates, index) =>{
+    if(coordinates.length > 10){
+      return "Límite de coordenadas superado. Edite la geocerca"
+      
+    }
+    
     let command = `(P29,1,${index},1,${coordinates.length}`;
     coordinates.map(c => {
       command += `,${ddddToDdmm(c.lng)},${ddddToDdmm(c.lat)}`;
@@ -37,13 +43,13 @@ const App = () => {
 }
 
   const _created = (e) => {  
-    const newPolygon = { _id: e.layer._leaflet_id};
-    newPolygon.coordinates = e.layer._latlngs[0].map((latlng) => ({
+    const newGeofence = { _id: e.layer._leaflet_id};
+    newGeofence.coordinates = e.layer._latlngs[0].map((latlng) => ({
       lat: latlng.lat,
       lng: latlng.lng,
     }));
 
-    setPolygons((polygons) => [...polygons, newPolygon]);  
+    setGeofences((geofences) => [...geofences, newGeofence]);  
     console.log(e);
     
   }
@@ -58,15 +64,15 @@ const App = () => {
       const editedIds = Object.values(_layers).map(({ _leaflet_id}) => _leaflet_id );
       console.log(editedIds);
       
-      setPolygons((polygons) => {
-        polygons.map( (p) =>{
+      setGeofences((geofences) => {
+        geofences.map( (g) =>{
           editedIds.map(id =>{
-            if(p._id === id){
-              p.coordinates = _layers[id]._latlngs[0];
+            if(g._id === id){
+              g.coordinates = _layers[id]._latlngs[0];
             }
           }) 
         }) 
-        return [...polygons];
+        return [...geofences];
       })
     }   
   };
@@ -79,15 +85,15 @@ const App = () => {
       const { layers: {_layers}} = e;
       const deletedIds = Object.values(_layers).map(({ _leaflet_id}) => _leaflet_id )
       
-      setPolygons((polygons) => {
-        polygons.map( (p) =>{
+      setGeofences((geofences) => {
+        geofences.map( (g) =>{
           deletedIds.map(id =>{
-            if(p._id === id){
-              polygons.splice(polygons.indexOf(p),1);
+            if(g._id === id){
+              geofences.splice(geofences.indexOf(g),1);
             }
           }) 
         }) 
-        return [...polygons];
+        return [...geofences];
       });     
     }
   };
@@ -95,7 +101,7 @@ const App = () => {
   useEffect(() => {
     editedRef.current = false;
     deletedRef.current = false;
-    console.log(polygons);
+    console.log(geofences);
   });
 
   return(
@@ -133,11 +139,21 @@ const App = () => {
       <div>
         <h3>Geofences Commands:</h3>
         <ul>
-          {polygons.map((polygon, index) => (
-            <li key={index}>
-              Geofence {index + 1}: {getCommand(polygon.coordinates, index+1)}
-            </li>
-          ))}
+          {geofences.map((geofence, index) => {
+            if(geofences.indexOf(geofence) < COMMANDS_QTY){
+              return(
+              <li key={index}>
+                Geofence {index + 1}: {getCommand(geofence.coordinates, index+1)}
+              </li>
+              )
+            }else{
+              return(
+              <li key={index}>
+                Geofence {index + 1}: Límite de comandos superado. Elimine las geocercas innecesarias
+              </li>
+              )  
+            }
+          })}
         </ul>
       </div>
     </>
