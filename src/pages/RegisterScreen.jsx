e9ac579ec7a2568f55aafb38bfa8b1a381022665
Ronny Acosta <racosta0254@gmail.com>
2024-09-12 00:19:59 -0400

    Validador de Registro

--- src/pages/RegisterScreen.jsx
+++ src/pages/RegisterScreen.jsx
@@ -2,15 +2,15 @@ import React, { useState } from 'react'
 
 const RegisterScreen = () => {
   const [userRegister, setUserRegister] = useState({
-    name: '',
-    lastName: '',
+    userName: '',
     email: '',
     password: '',
     confirmPassword: ''
   })
-  const {name, lastName, email, password, confirmPassword} = userRegister;
+  const {userName, email, password, confirmPassword} = userRegister;
   
-  const handleRegister = (e) =>{
+  const handleChange = (e) =>{
+    e.target.nextSibling.nextSibling.style.display = 'none'
     setUserRegister({
       ...userRegister,
       [e.target.name]: e.target.value
@@ -19,7 +19,36 @@ const RegisterScreen = () => {
   
   const handleSubmit = (e) =>{
     e.preventDefault()
-    console.log(userRegister)
+    const validator = {
+      confirm: true
+    };
+    
+    const error = (prop) =>{
+      document.querySelector(prop).previousElementSibling.style.color = 'red'
+      document.querySelector(prop).style.borderBottom = '2px solid red';
+      document.querySelector(prop).nextSibling.style.color = 'red';
+      document.querySelector(prop).nextSibling.nextSibling.style.display = 'block';
+      validator.confirm = false;
+      console.log(validator)
+    }
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if(userName.length < 3){
+      error('#userName')
+    }
+
+    if(emailRegex.test(email) === false){
+      error('#email')
+    }
+    if(password.length < 8){
+      error('#password');
+    } else if(confirmPassword !== password){
+        error('#confirmPassword');
+      }
+
+    if(validator.confirm === true){
+      console.log(userRegister)
+    }
   }
   const handleFocus = (e) =>{
     e.target.previousElementSibling.style.color = '#07bcff'
@@ -27,6 +56,8 @@ const RegisterScreen = () => {
 
   const handleBlur = (e) =>{
     e.target.previousElementSibling.style.color = '#000'
+    e.target.style.borderBottom = '1px solid #9e9e9e'
+    e.target.nextSibling.style.color = '#9e9e9e'
   }
   
   return (
@@ -34,29 +65,33 @@ const RegisterScreen = () => {
       <h1 className='title'>JT701 - Geofence Commands Generator</h1>
       <div className='container'>
         <h3>Register</h3>
-        <div className="divider"></div>
+        <div className="divider mb-4"></div>
         <div className="row container">
           <form className="col s12" method='post' onSubmit={handleSubmit}>
             <div className="row">
               <div className="input-field col s12">
                 <i className="material-icons prefix">person</i>
-                <input id="userName" name='userName' type="text" className="validate" value={name} onBlur={handleBlur} onFocus={handleFocus} onChange={handleRegister} />
+                <input id="userName" name='userName' type="text" className="validate" value={userName} onBlur={handleBlur} onFocus={handleFocus} onChange={handleChange} />
                 <label htmlFor="userName">User Name</label>
+                <div className='center error-message'>Min lenght: 3</div>
               </div>
               <div className="input-field col s12">
                 <i className="material-icons prefix">email</i>
-                <input id="email" name='email' type="email" className="validate" value={email} onBlur={handleBlur} onFocus={handleFocus} onChange={handleRegister} />
+                <input id="email" name='email' type="text" className="validate" value={email} onBlur={handleBlur} onFocus={handleFocus} onChange={handleChange} />
                 <label htmlFor="email">Email</label>
+                <div className='center error-message'>Invalid email</div>
               </div>
               <div className="input-field col s12">
                 <i className="material-icons prefix">vpn_key</i>
-                <input id="password" name='password' type="password" className="validate" onBlur={handleBlur} onFocus={handleFocus} value={password} onChange={handleRegister} />
+                <input id="password" name='password' type="password" className="validate" onBlur={handleBlur} onFocus={handleFocus} value={password} onChange={handleChange}  />
                 <label htmlFor="password">Password</label>
+                <div className='center error-message'>Min lenght: 8</div>
               </div>
               <div className="input-field col s12">
                 <i className="material-icons prefix">vpn_key</i>
-                <input id="confirmPassword" name='confirmPassword' type="password" className="validate" onBlur={handleBlur} onFocus={handleFocus} value={confirmPassword} onChange={handleRegister} />
+                <input id="confirmPassword" name='confirmPassword' type="password" className="validate" onBlur={handleBlur} onFocus={handleFocus} value={confirmPassword} onChange={handleChange}  />
                 <label htmlFor="confirmPassword">Confirm password</label>
+                <div className='center error-message'>Wrong Password</div>
               </div>
               <button type='submit' className='btn col s12 blue waves-effect waves-light'>Register</button>
             </div>
